50293af03c9303b405d63039a1089d2beb2c5a3c
gnzlplcs <palaciosgm@outlook.com>
2022-09-15 17:10:32 -0500

    :sparkles: create Header component
    add Header component into the Layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,13 @@
 import styles from '../styles/Layout.module.css';
 import Nav from './Nav';
+import Header from './Header';
 
 const Layout = ({children}) => {
   return (
     <div className={styles.container}>
       <Nav />
       <main className={styles.main}>
+        <Header />
         {children}
       </main>
     </div>
