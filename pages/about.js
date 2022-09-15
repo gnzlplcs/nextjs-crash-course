9c7e83320b2eb21337ca1709e29d4fbfe4489fc5
gnzlplcs <palaciosgm@outlook.com>
2022-09-15 16:25:06 -0500

    :construction: create About component

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const About = () => {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+      <h1>About</h1>
+
+    </div>
+  )
+}
+
+export default About
