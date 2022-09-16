8691f5867299d215507743b532af997ad3873481
gnzlplcs <palaciosgm@outlook.com>
2022-09-15 22:40:42 -0500

    :truck: set dynamic routes
    use getStaticProps and setStaticPaths

---
+++ pages/article/[id]/index.js
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+const Article = ({ article }) => {
+  return (
+    <div>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href="/">Go Back</Link>
+    </div>
+  );
+};
+
+export const getStaticProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const articles = await res.json();
+  const ids = articles.map((article) => article.id);
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export default Article;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
-import Head from 'next/head'
+import Head from "next/head";
+import ArticleList from "../components/ArticleList";
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
         <title>WebDev News</title>
-        <meta name="keywords" content='web development, programming'/>
+        <meta name="keywords" content="web development, programming" />
       </Head>
-      <h1>Welcome to Next</h1>
+      <ArticleList articles={articles}/>
     </div>
-  )
+  );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_limit=6"
+  );
+  const articles = await res.json();
+  
+  return {
+    props: {
+      articles,
+    },
+  };
+};
